9a9e22889448080fb9411af188ee033ccb89bbde
Devina Ekawati <devina.ekawati@gmail.com>
2017-03-21 18:48:19 +0700

    Move get features into functions

--- src/aspect extractor/crf_data_generator.py
+++ src/aspect extractor/crf_data_generator.py
@@ -9,7 +9,7 @@ class CRFDataGenerator:
 		self.list_unigrams = []
 		self.list_bigrams = []
 		self.list_trigrams = []
-		self.list_pos_tag_trigams = []
+		self.list_pos_tag_trigrams = []
 		self.CONLL_table = CONLLTable("../../data/output1.conll")
 
 	def init_dependency_tags(self):
@@ -31,7 +31,7 @@ class CRFDataGenerator:
 		return self.list_trigrams
 
 	def get_list_pos_tag_trigrams(self):
-		return self.list_pos_tag_trigams
+		return self.list_pos_tag_trigrams
 
 	def get_window_text(self, n, tokens, pos):
 		center = int(n/2) + 1
@@ -57,68 +57,69 @@ class CRFDataGenerator:
 			ngram = Counter(ngrams(sentence.split(), n))
 			for key in ngram:
 				if key not in result:
-					result[key] = ngram[key]
+					if (n == 1):
+						result[key[0]] = ngram[key]
+					else:
+						result[key] = ngram[key]
 				else:
-					result[key] += ngram[key]
+					if (n == 1):
+						result[key[0]] += ngram[key]
+					else:
+						result[key] += ngram[key]
 		return result
 
-	def get_feature(self, id_sentence, id_word):
-		# bag_of_vbot = self.init_dependency_tags()
-		row = self.CONLL_table.get_row(id_sentence, id_word)
-		line = self.CONLL_table.get_word(row) + " " + self.CONLL_table.get_pos_tag(row)
+	def get_n_grams_feature(self, n, window_text, list_n_grams):
+		line = ""
+		window_ngrams = Counter(ngrams(nltk.word_tokenize(window_text), n))
 
-		label = self.CONLL_table.get_label(row)
+		for n_grams in list_n_grams:
+			if (n_grams in window_ngrams):
+				line += " " + str(window_ngrams[n_grams])
+			else:
+				line += " 0"
+		return line
+
+	def get_dependency_tags_feature(self, id_sentence):
+		bag_of_vbot = self.init_dependency_tags()
+		line = ""
+		filter = ["VERB"]
+		words = self.CONLL_table.filter_words_by_pos_tag(id_sentence, filter)
+		for key1 in words:
+			children = self.CONLL_table.get_children(key1, id_sentence)
+			for key2, value2 in children.iteritems():
+				bag_of_vbot[self.CONLL_table.get_tree_tag(value2)] += 1
+
+			siblings = self.CONLL_table.get_siblings(key1, id_sentence)
+			for key2, value2 in siblings.iteritems():
+				bag_of_vbot[self.CONLL_table.get_tree_tag(value2)] += 1
+
+		for key, value in bag_of_vbot.iteritems():
+			line += " " + str(value)
+
+		return line
+
+	def get_dict_feature(self, label):
+		line = ""
 		if (label == "ASPECT-B" or label == "ASPECT-I"):
 			line += " yes"
 		else:
 			line += " no"
+		return line
+
+	def get_feature(self, id_sentence, id_word):
+		row = self.CONLL_table.get_row(id_sentence, id_word)
+		line = self.CONLL_table.get_word(row) + " " + self.CONLL_table.get_pos_tag(row)
+
+		label = self.CONLL_table.get_label(row)
 		
 		window_text = self.get_window_text(5, self.CONLL_table.get_sentence(id_sentence).split(), id_word)
-		window_unigrams = Counter(ngrams(nltk.word_tokenize(window_text), 1))
-		window_bigrams = Counter(ngrams(nltk.word_tokenize(window_text), 2))
-		window_trigrams = Counter(ngrams(nltk.word_tokenize(window_text), 3))
+		
+		line += self.get_n_grams_feature(1, window_text, self.list_unigrams)
+		line += self.get_n_grams_feature(2, window_text, self.list_bigrams)
+		line += self.get_n_grams_feature(3, window_text, self.list_trigrams)
 
 		window_pos_tag = self.get_window_text(5, self.CONLL_table.get_sentence_pos_tag(id_sentence).split(), id_word)
-		window_pos_tag_trigrams = Counter(ngrams(nltk.word_tokenize(window_pos_tag), 3))
-
-		for word in self.list_unigrams:
-			key = (word,)
-			if (key in window_unigrams):
-				line += " " + str(window_unigrams[key])
-			else:
-				line += " 0"
-
-		for bigrams in self.list_bigrams:
-			if (bigrams in window_bigrams):
-				line += " " + str(window_bigrams[bigrams])
-			else:
-				line += " 0"
-
-		for trigrams in self.list_trigrams:
-			if (trigrams in window_trigrams):
-				line += " " + str(window_trigrams[trigrams])
-			else:
-				line += " 0"
-
-		for trigrams in self.list_pos_tag_trigams:
-			if (trigrams in window_pos_tag_trigrams):
-				line += " " + str(window_pos_tag_trigrams[trigrams])
-			else:
-				line += " 0"
-
-		# filter = ["VERB"]
-		# words = self.CONLL_table.filter_words_by_pos_tag(id_sentence, filter)
-		# for key1 in words:
-		# 	children = self.CONLL_table.get_children(key1, id_sentence)
-		# 	for key2, value2 in children.iteritems():
-		# 		bag_of_vbot[self.CONLL_table.get_tree_tag(value2)] += 1
-
-		# 	siblings = self.CONLL_table.get_siblings(key1, id_sentence)
-		# 	for key2, value2 in siblings.iteritems():
-		# 		bag_of_vbot[self.CONLL_table.get_tree_tag(value2)] += 1
-
-		# for key, value in bag_of_vbot.iteritems():
-		# 	line += " " + str(value)
+		line += self.get_n_grams_feature(3, window_pos_tag, self.list_pos_tag_trigrams)
 
 		return line + " " + label + "\n"
 
@@ -126,7 +127,7 @@ class CRFDataGenerator:
 		reviews = self.CONLL_table.get_sentences(start, end)
 
 		filter = ["NOUN", "ADJ", "ADV", "VERB"]
-		unigrams = Counter(ngrams(nltk.word_tokenize(" ".join(self.CONLL_table.get_filtered_sentences(filter))), 1))
+		unigrams = self.get_n_grams(1, self.CONLL_table.get_filtered_sentences(filter))
 
 		bigrams = [b for l in reviews for b in zip(l.split(" ")[:-1], l.split(" ")[1:])]
 		bigrams = Counter(bigrams)
@@ -135,7 +136,7 @@ class CRFDataGenerator:
 		trigrams_postag = self.get_n_grams(3, self.CONLL_table.get_sentences_pos_tag())
 
 		for key in unigrams:
-			self.list_unigrams.append(key[0])
+			self.list_unigrams.append(key)
 
 		for key in bigrams:
 			self.list_bigrams.append(key)
@@ -144,7 +145,7 @@ class CRFDataGenerator:
 			self.list_trigrams.append(key)
 
 		for key in trigrams_postag:
-			self.list_pos_tag_trigams.append(key)
+			self.list_pos_tag_trigrams.append(key)
 
 		with open(filename, 'w') as f:
 			for i in range(self.CONLL_table.get_sentences_size()):
--- src/aspect extractor/crfsuite_data_generator.py
+++ src/aspect extractor/crfsuite_data_generator.py
@@ -8,10 +8,10 @@ from collections import Counter
 separator = ' '
 
 # Field names of the input data.
-fields = 'w pos dict'
+fields = 'w pos'
 
-# templates = [(('w', 0),)]
-templates = [(('w', 0),), (('dict', 0),)]
+templates = [(('w', 0),)]
+# templates = [(('w', 0),), (('dict', 0),)]
 
 def generate_templates_from_file(filename, symbol):
 	global fields, templates
@@ -28,9 +28,9 @@ def generate_templates_from_file(filename, symbol):
 def generate_templates(unigram_filename, bigram_filename, ngram_filename, ngram_pos_tag_filename, dependency_tags_filename):
 	global fields
 	generate_templates_from_file(unigram_filename, "U")
-	generate_templates_from_file(bigram_filename, "B")
-	generate_templates_from_file(ngram_filename, "T")
-	generate_templates_from_file(ngram_pos_tag_filename, "TP")
+	# generate_templates_from_file(bigram_filename, "B")
+	# generate_templates_from_file(ngram_filename, "T")
+	# generate_templates_from_file(ngram_pos_tag_filename, "TP")
 	# generate_templates_from_file(dependency_tags_filename, "D")
 
 	fields += ' y'
