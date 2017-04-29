Add ratings calculation and fix mapping in sentimen extractor

src/category_extractor/category_extractor.py
@@ -47,21 +47,30 @@ class CategoryExtractor:
 
             ('clf', OneVsRestClassifier(LogisticRegression()))
         ])
+
+        self.model_filename = os.path.join(project_path, "../data/category_extraction/category_extractor.model")
+        stopword_filename = os.path.join(project_path, "preprocess/resource/stopword.txt")
+
+        with open(stopword_filename, "r") as f:
+            stopword = f.readlines()
+        self.stopword = [x.rstrip() for x in stopword]
+
         self.target_names = ['food', 'service', 'price', 'place']
         train_data, self.train_target = self.read_data(os.path.join(project_path, "../data/category_extraction/train_data.csv"))
         self.train_data = np.array(train_data)
-        
-        self.model_filename = os.path.join(project_path, "../data/category_extraction/category_extractor.model")
 
     def read_data(self, filename):
         data = []
         targets = []
         regex = re.compile('[^0-9a-zA-Z]+')
+
         with open (filename, "rb") as f:
             reader = csv.reader(f, delimiter=';', quotechar='"')
             next(reader)
             for row in reader:
-                data.append(regex.sub(' ', row[0]))
+                text = regex.sub(' ', row[0])
+                # text = " ".join(x for x in text.split() if x not in self.stopword)
+                data.append(text)
                 target = []
                 for i in range(1, len(self.target_names) + 1):
                     if (row[i] == "yes"):
@@ -83,9 +92,9 @@ class CategoryExtractor:
         X_folds = np.array_split(self.train_data, n)
         y_folds = np.array_split(labels, n)
 
-        precision_scores = []
-        recall_scores = []
-        f1_scores = []
+        precision_scores = [[], [], [], []]
+        recall_scores = [[], [], [], []]
+        f1_scores = [[], [], [], []]
 
         for k in range(n):
             X_train = list(X_folds)
@@ -99,14 +108,16 @@ class CategoryExtractor:
             predicted = self.pipeline.predict(X_test)
 
             # print classification_report(y_test, predicted, target_names=self.target_names)
+            for i in range(4):
+                precision_scores[i].append(precision_score(y_test, predicted, average=None)[i])
+                recall_scores[i].append(recall_score(y_test, predicted, average=None)[i])
+                f1_scores[i].append(f1_score(y_test, predicted, average=None)[i])
 
-            precision_scores.append(precision_score(y_test, predicted, average=None).mean())
-            recall_scores.append(recall_score(y_test, predicted, average=None).mean())
-            f1_scores.append(f1_score(y_test, predicted, average=None).mean())
-
-        print "Precision: ", np.array(precision_scores).mean()
-        print "Recall: ", np.array(recall_scores).mean()
-        print "F1-score: ", np.array(f1_scores).mean()
+        for i in range(4):
+            print "Category: ", self.target_names[i]
+            print "\tPrecision: ", np.array(precision_scores[i]).mean()
+            print "\tRecall: ", np.array(recall_scores[i]).mean()
+            print "\tF1-score: ", np.array(f1_scores[i]).mean()
 
     def evaluate(self, test_filename):
         # self.train()
@@ -119,9 +130,13 @@ class CategoryExtractor:
 
         predicted = model.predict(test_data)
 
-        print "Precision: ", precision_score(labels, predicted, average=None)
-        print "Recall: ", recall_score(labels, predicted, average=None)
-        print "F1-score: ", f1_score(labels, predicted, average=None)
+        print "Precision: ", np.array(precision_score(labels, predicted, average=None))
+        print "Recall: ", np.array(recall_score(labels, predicted, average=None))
+        print "F1-score: ", np.array(f1_score(labels, predicted, average=None))
+
+        # print "Precision: ", precision_score(labels, predicted, average=None)
+        # print "Recall: ", recall_score(labels, predicted, average=None)
+        # print "F1-score: ", f1_score(labels, predicted, average=None)
 
         all_labels = mlb.inverse_transform(predicted)
 
@@ -161,7 +176,7 @@ class CategoryExtractor:
 
 if __name__ == '__main__':
     category_extractor = CategoryExtractor()
-    category_extractor.train()
+    # category_extractor.train()
     # category_extractor.evaluate_cross_validation()
     # category_extractor.evaluate("../../data/category_extraction/test_data.csv")
     category_extractor.evaluate("../../data/category_extraction/test_data_cumulative.csv")

src/category_extractor/category_feature_extractor.py
@@ -13,11 +13,11 @@ class CategoryFeatureExtractor(BaseEstimator, TransformerMixin):
         features = np.recarray(shape=(len(sentences),),
                                dtype=[('sentence', object), ('word2vec', object), ('glove', object), ('lda', object)], )
         word2vec_cluster = self.read_word_embedding_cluster(
-            os.path.join(project_path, "../data/word_embedding/word2vec_cluster_100.txt"))
+            os.path.join(project_path, "../data/word_embedding/word2vec_cluster_5000.txt"))
         glove_cluster = self.read_word_embedding_cluster(
-            os.path.join(project_path, "../data/word_embedding/glove_cluster_100.txt"))
+            os.path.join(project_path, "../data/word_embedding/glove_cluster_5000.txt"))
         lda_cluster = self.read_word_embedding_cluster(
-            os.path.join(project_path, "../data/word_embedding/lda_cluster_100.txt"))
+            os.path.join(project_path, "../data/word_embedding/lda100_cluster_5000.txt"))
         for i, text in enumerate(sentences):
             features[i]['sentence'] = text
             features[i]['word2vec'] = self.get_word_embedding(text, word2vec_cluster)

src/sentiment_extractor/sentiment_extractor.py
@@ -17,11 +17,19 @@ class SentimentExtractor:
         file_path = os.path.dirname(os.path.abspath(__file__))
         project_path = os.path.abspath(os.path.join(file_path, os.path.pardir))
 
+        stopword_filename = os.path.join(project_path, "preprocess/resource/stopword.txt")
+
+        with open(stopword_filename, "r") as f:
+            stopword = f.readlines()
+        self.stopword = [x.rstrip() for x in stopword]
+
         self.categories = ['food', 'service', 'price', 'place']
         self.target_names = ['positive', 'negative', 'neutral']
+        # self.target_names = ['positive', 'negative']
         self.train_data = []
         self.train_target = []
-        self.train_data, train_target, all_data = self.read_data(os.path.join(project_path, "../data/sentiment_extraction/train_data.csv"))
+        self.train_data, train_target, all_data = self.read_data(
+            os.path.join(project_path, "../data/sentiment_extraction/train_data.csv"))
         for i in range(len(self.categories)):
             # self.train_data.append(np.array(train_data[i]))
             self.train_target.append(np.array(train_target[i]))
@@ -34,7 +42,7 @@ class SentimentExtractor:
 
     def read_data(self, filename):
         all_data = []
-        data = [{}, {}, {}, {}]
+        data = [OrderedDict(), OrderedDict(), OrderedDict(), OrderedDict()]
         targets = [[], [], [], []]
         regex = re.compile('[^0-9a-zA-Z]+')
         with open(filename, "rb") as f:
@@ -42,23 +50,27 @@ class SentimentExtractor:
             next(reader)
             j = 0
             for row in reader:
-                all_data.append(regex.sub(' ', row[0]))
+                text = regex.sub(' ', row[0])
+                # text = " ".join(x for x in text.split() if x not in self.stopword)
+
+                all_data.append(regex.sub(' ', text))
                 for i in range(1, len(self.categories) + 1):
                     if row[i] != "-":
-                        data[i - 1][j] = regex.sub(' ', row[0])
+                        data[i - 1][j] = regex.sub(' ', text)
                         targets[i - 1].append(self.target_names.index(row[i]))
                 j += 1
+
         return data, targets, all_data
 
     def get_pipeline(self, index):
         skipper_2 = functools.partial(skipgrams, n=2, k=2)
         vocabulary_skipper_2 = self.get_skip_bigrams_vocabulary(2, 5, index)
-        # skipper_3 = functools.partial(skipgrams, n=2, k=3)
-        # vocabulary_skipper_3 = self.get_skip_bigrams_vocabulary(3, 5, index)
-        # skipper_4 = functools.partial(skipgrams, n=2, k=4)
-        # vocabulary_skipper_4 = self.get_skip_bigrams_vocabulary(4, 5, index)
-        # skipper_5 = functools.partial(skipgrams, n=2, k=5)
-        # vocabulary_skipper_5 = self.get_skip_bigrams_vocabulary(5, 5, index)
+        skipper_3 = functools.partial(skipgrams, n=2, k=3)
+        vocabulary_skipper_3 = self.get_skip_bigrams_vocabulary(3, 5, index)
+        skipper_4 = functools.partial(skipgrams, n=2, k=4)
+        vocabulary_skipper_4 = self.get_skip_bigrams_vocabulary(4, 5, index)
+        skipper_5 = functools.partial(skipgrams, n=2, k=5)
+        vocabulary_skipper_5 = self.get_skip_bigrams_vocabulary(5, 5, index)
 
         return Pipeline([
             ('data', SentimentFeatureExtractor()),
@@ -77,19 +89,20 @@ class SentimentExtractor:
                     # ])),
 
                     # ('bag_of_skipbigram_2', Pipeline([
+                    #     ('selector', ItemSelector(key='sentence')),
                     #     ('ngram', CountVectorizer(analyzer=skipper_2, vocabulary=vocabulary_skipper_2)),
                     # ])),
-
+                    #
                     # ('bag_of_skipbigram_3', Pipeline([
                     #     ('selector', ItemSelector(key='sentence')),
                     #     ('ngram', CountVectorizer(analyzer=skipper_3, vocabulary=vocabulary_skipper_3)),
                     # ])),
-
+                    #
                     # ('bag_of_skipbigram_4', Pipeline([
                     #     ('selector', ItemSelector(key='sentence')),
                     #     ('ngram', CountVectorizer(analyzer=skipper_4, vocabulary=vocabulary_skipper_4)),
                     # ])),
-
+                    #
                     # ('bag_of_skipbigram_5', Pipeline([
                     #     ('selector', ItemSelector(key='sentence')),
                     #     ('ngram', CountVectorizer(analyzer=skipper_5, vocabulary=vocabulary_skipper_5)),
@@ -151,6 +164,7 @@ class SentimentExtractor:
 
             pipeline = self.get_pipeline(index)
 
+            print "CATEGORY: " + self.categories[index]
             for k in range(n):
                 X_train = list(X_folds)
                 X_test = X_train.pop(k)
@@ -160,15 +174,15 @@ class SentimentExtractor:
                 y_train = np.concatenate(y_train)
 
                 model = pipeline.fit(X_train, y_train)
-                predicted = pipeline.predict(X_test)
+                predicted = model.predict(X_test)
 
                 # print classification_report(y_test, predicted)
+                # print confusion_matrix(y_test, predicted)
 
                 precision_scores.append(precision_score(y_test, predicted, average=None).mean())
                 recall_scores.append(recall_score(y_test, predicted, average=None).mean())
                 f1_scores.append(f1_score(y_test, predicted, average=None).mean())
 
-            print "CATEGORY: " + self.categories[index]
             print "\tPrecision: ", np.array(precision_scores).mean()
             print "\tRecall: ", np.array(recall_scores).mean()
             print "\tF1-score: ", np.array(f1_scores).mean()
@@ -176,33 +190,24 @@ class SentimentExtractor:
     def evaluate(self, test_filename):
         test_data, test_target, all_data = self.read_data(test_filename)
 
-        predictions = [{}, {}, {}, {}]
         for i in range(4):
             model = joblib.load(self.model_filenames[i])
             predicted = model.predict(test_data[i].values())
 
-            # print "CATEGORY: " + self.categories[i]
-            # print "\tPrecision: ", np.array(precision_score(test_target[i], predicted, average=None)).mean()
-            # print "\tRecall: ", np.array(recall_score(test_target[i], predicted, average=None)).mean()
-            # print "\tF1-score: ", np.array(f1_score(test_target[i], predicted, average=None)).mean()
-            k = 0
-            for j in range(len(all_data)):
-                if j in test_data[i]:
-                    predictions[i][j] = self.target_names[predicted[k]]
-                    k += 1
-                    # print "\t", j, test_data[i][j], self.target_names[predicted[j]]
+            if i == 1:
+                for key, actual, predict in zip(test_data[i], test_target[i], predicted):
+                    if actual != predict:
+                        print key+1, " ", test_data[i][key]
+                        print "\tactual: ", actual
+                        print "\tpredict: ", predict
 
-        with open("../../data/sentiment_extraction/result_test_data_cumulative.csv", "wb") as f:
-            writer = csv.writer(f, delimiter=';', quotechar='"')
-            writer.writerow(["sentence", "food", "service", "price", "place"])
-            for i, sentence in enumerate(all_data):
-                data = [sentence, "-", "-", "-", "-"]
-                for j in range(4):
-                    if i in test_data[j]:
-                        if data[0] == "-":
-                            data[0] = test_data[j][i]
-                        data[j+1] = predictions[j][i]
-                writer.writerow(data)
+            print "CATEGORY: " + self.categories[i]
+            print "\tPrecision: ", np.array(precision_score(test_target[i], predicted, average=None)).mean()
+            print "\tRecall: ", np.array(recall_score(test_target[i], predicted, average=None)).mean()
+            print "\tF1-score: ", np.array(f1_score(test_target[i], predicted, average=None)).mean()
+
+            # print classification_report(test_target[i], predicted)
+            # print confusion_matrix(test_target[i], predicted)
 
     def evaluate_accumulative(self, actual_data_filename, test_filename):
         actual_data, actual_target, all_data = self.read_data(actual_data_filename)
@@ -214,21 +219,53 @@ class SentimentExtractor:
             predicted = model.predict(test_data[i].values())
 
             test_data[i] = OrderedDict(test_data[i])
-            correct = 0.0
-            for key, target in zip(actual_data[i], actual_target[i]):
-                if key in test_data[i]:
-                    if target == predicted[test_data[i].keys().index(key)]:
-                        correct += 1
-            precision = correct / len(test_target[i])
-            recall = correct/len(actual_target[i])
-            f1 = 0
-            if recall != 0:
-                f1 = (2 * precision * recall) / (precision + recall)
+            class_precision = []
+            class_recall = []
+            class_f1 = []
+            for j in range(len(self.target_names)):
+                correct = 0.0
+                size_class_prediction = 0
+                size_class_actual = 0
 
-            print "\tPrecision: ", precision
-            print "\tRecall: ", recall
-            print "\tF1-score: ", f1
+                for target in actual_target[i]:
+                    if target == j:
+                        size_class_actual += 1
 
+                for predict in predicted:
+                    if predict == j:
+                        size_class_prediction += 1
+
+                for key, target in zip(actual_data[i], actual_target[i]):
+                    if key in test_data[i]:
+                        prediction = predicted[test_data[i].keys().index(key)]
+                        if prediction == j:
+                            if target == prediction:
+                                correct += 1
+
+                precision = 0
+                recall = 0
+                f1 = 0
+
+                if size_class_prediction > 0:
+                    precision = correct / size_class_prediction
+
+                if size_class_actual > 0:
+                    recall = correct / size_class_actual
+
+                if recall > 0:
+                    f1 = (2 * precision * recall) / (precision + recall)
+
+                class_precision.append(precision)
+                class_recall.append(recall)
+                class_f1.append(f1)
+
+            # print "\tPrecision: ", class_precision
+            # print "\tRecall: ", class_recall
+            # print "\tF1-score: ", class_f1
+
+            print "\tPrecision: ", np.array(class_precision).mean()
+            print "\tRecall: ", np.array(class_recall).mean()
+            print "\tF1-score: ", np.array(class_f1).mean()
 
     def predict(self, category, test_data):
         results = []
@@ -247,11 +284,12 @@ class SentimentExtractor:
 
         return results
 
+
 if __name__ == '__main__':
     sentiment_extractor = SentimentExtractor()
-    # sentiment_extractor.train()
-    # sentiment_extractor.evaluate_cross_validation()
+    sentiment_extractor.train()
+    sentiment_extractor.evaluate_cross_validation()
 
-    # sentiment_extractor.evaluate("../../data/sentiment_extraction/test_data_cumulative.csv")
-    sentiment_extractor.evaluate_accumulative("../../data/sentiment_extraction/test_data.csv",
-                                              "../../data/sentiment_extraction/test_data_cumulative.csv")
+    # sentiment_extractor.evaluate("../../data/sentiment_extraction/test_data.csv")
+    # sentiment_extractor.evaluate_accumulative("../../data/sentiment_extraction/test_data.csv",
+    #                                           "../../data/sentiment_extraction/test_data_cumulative.csv")

src/sentiment_extractor/sentiment_feature_extractor.py
@@ -16,10 +16,10 @@ class SentimentFeatureExtractor(BaseEstimator, TransformerMixin):
         features = np.recarray(shape=(len(sentences),),
                                dtype=[('sentence', object), ('headword', object), ('word2vec', object),
                                       ('glove', object)], )
-        word2vec_cluster = self.read_word_embedding_cluster(os.path.join(project_path, "../data/word_embedding/word2vec_cluster_100.txt"))
-        glove_cluster = self.read_word_embedding_cluster(os.path.join(project_path, "../data/word_embedding/glove_cluster_100.txt"))
+        word2vec_cluster = self.read_word_embedding_cluster(os.path.join(project_path, "../data/word_embedding/word2vec_cluster_5000.txt"))
+        glove_cluster = self.read_word_embedding_cluster(os.path.join(project_path, "../data/word_embedding/glove_cluster_500.txt"))
         for i, text in enumerate(sentences):
-            features[i]['headword'] = CONLL_table.get_head_word_of_sentence(i)
+            # features[i]['headword'] = CONLL_table.get_head_word_of_sentence(i)
             features[i]['sentence'] = text
             features[i]['word2vec'] = self.get_word_embedding(text, word2vec_cluster)
             features[i]['glove'] = self.get_word_embedding(text, glove_cluster)